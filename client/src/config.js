efca49a0d21b79a9f0fbe5d2b90a63d24f1067cb
anshumanraj20 <anshumanraj2003@gmail.com>
2024-03-04 13:39:52 +0530

    updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://socialify-phi.vercel.app/";
+let BASE_URL = "https://socialify-ch3i.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://socialify-phi.vercel.app/"],
+    origin: ["http://localhost:3000", "https://socialify-ch3i.onrender.com/"],
   },
 });
 
@@ -43,10 +43,10 @@ app.use("/api/users", users);
 app.use("/api/comments", comments);
 app.use("/api/messages", messages);
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
